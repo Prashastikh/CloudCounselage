bf0c1bc0f1ba3ec95093efdbbc769895b4ec5309
Swagat Ahire <31353464+swagat11@users.noreply.github.com>
2020-10-02 23:08:26 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,31 @@
-import React from 'react';
-import './App.css';
+import React, { useState } from 'react';
 
-function App() {
+const App =()=>{
+  let newTime = new Date().toLocaleTimeString();
+  const[ctime,setCtime]=useState();
+
+  let newDate = new Date().toLocaleDateString();
+  const[cdate,setCdate]=useState();
+  
+  
+  const UpdateTime = () => {
+     newTime = new Date().toLocaleTimeString();
+    setCtime(newTime);
+
+    newDate = new Date().toLocaleDateString();
+    setCdate(newDate);
+
+
+  };
   return (
-      <div className="App-header">
-        Every Moment Counts
-      </div>
+    <>
+    <h1>Welcome To local Time System</h1>
+    <h2>Current Time : {ctime}</h2>
+    <h2>Current Date : {cdate}</h2>
+    <button onClick={UpdateTime}>get time & date</button>
+</>
   );
 }
-export default App;
 
+
+export default App;
